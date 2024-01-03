f1969d4148610516b7669cb7891ce909b8ae3697
Carlos Santos <cadu.vs@hotmail.com>
2024-01-03 14:39:10 -0300

    fix: relevant functions are now exposed

--- ezio/__init__.py
+++ ezio/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "0.1.0"
+from .config import load_config, set_engine
+from .facade import load_table, save_table
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "load_config",
+    "set_engine",
+    "load_table",
+    "save_table",
+]
