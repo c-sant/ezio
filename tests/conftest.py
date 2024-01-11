4493df659aaa7cf203742d749fc68b9b1bfed7a4
Carlos Santos <cadu.vs@hotmail.com>
2024-01-11 10:02:41 -0300

    fix: test toml config files are no longer incorrectly indented

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import pytest
 
 
@@ -7,13 +9,57 @@ def sample_config_path_fixture() -> str:
 
     with open(config_path, "w", encoding="utf-8") as file:
         file.write(
-            """
+            textwrap.dedent(
+                """
             [element1]
             key1 = "value1"
             
             [element2]
             key2 = "value2"
             """
+            )
+        )
+
+    return config_path
+
+
+@pytest.fixture(name="config_path")
+def config_path_fixture() -> str:
+    config_path = "config.toml"
+
+    with open(config_path, "w", encoding="utf-8") as file:
+        file.write(
+            textwrap.dedent(
+                """
+            [sources.localStorage]
+            type = "localFileSystem"
+
+            [test.excel]
+            path = "data.xlsx"
+            source = "localStorage"
+            format = "excel"
+
+            [test.csv]
+            path = "data.csv"
+            source = "localStorage"
+            format = "csv"
+
+            [test.text]
+            path = "data.txt"
+            source = "localStorage"
+            format = "txt"
+
+            [test.json]
+            path = "data.json"
+            source = "localStorage"
+            format = "json"
+
+            [test.parquet]
+            path = "data.parquet"
+            source = "localStorage"
+            format = "parquet"
+            """
+            )
         )
 
     return config_path
